9c2ffd080614b4605d841a3c1be42e3475a35ce1
Victor Garcia Mestre <vgmestre@gmail.com>
2022-07-11 22:09:54 +0200

    task list

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -10,17 +10,56 @@ import {
   useColorMode,
   useColorModeValue
 } from 'native-base'
+import { AntDesign } from '@expo/vector-icons'
 import ThemeToggle from '../components/ThemeToggle'
 import AnimatedCheckbox from '../components/AnimatedCheckbox'
 import TaskItem from '../components/TaskItem'
+import TaskList from '../components/TaskList'
+import SwipableView from '../components/SwipableView'
+import shortid from 'shortid'
+
+const initialData = [
+  {
+    id: shortid.generate(),
+    subject: 'buy tickets',
+    done: false
+  },
+  { id: shortid.generate(), subject: 'Make tutorial', done: false }
+]
 
 const Main = () => {
-  const [checked, setChecked] = useState(false)
-  const [subject, setSubject] = useState('task item')
-  const [isEditing, setIsEditing] = useState(false)
-  const handlePressCheckbox = useCallback(() => {
-    setChecked(prev => !prev)
+  const [data, setData] = useState(initialData)
+  const [editingItemId, setEditingItemId] = useState<string | null>(null)
+  const handleToggleTaskItem = useCallback(item => {
+    setData(prevData =>
+      prevData.map(prevItem => {
+        if (prevItem.id === item.id) {
+          return { ...prevItem, done: !prevItem.done }
+        }
+        return prevItem
+      })
+    )
   }, [])
+  const handleChangeTaskItemSubject = useCallback((item, newSubject) => {
+    setData(prevData =>
+      prevData.map(prevItem => {
+        if (prevItem.id === item.id) {
+          return { ...prevItem, subject: newSubject }
+        }
+        return prevItem
+      })
+    )
+  }, [])
+  const handleFinishEditingTaskItem = useCallback(_item => {
+    setEditingItemId(null)
+  }, [])
+  const handlePressTaskItemLabel = useCallback(item => {
+    setEditingItemId(item.id)
+  }, [])
+  const handleRemoveItem = useCallback(item => {
+    setData(prevData => prevData.filter(i => i !== item))
+  }, [])
+
   return (
     <Center
       _dark={{ bg: 'blueGray.900' }}
@@ -28,14 +67,14 @@ const Main = () => {
       flex={1}
     >
       <VStack space={5} alignItems="center" w="full">
-        <TaskItem
-          isEditing={isEditing}
-          isDone={checked}
-          onToggleCheckbox={handlePressCheckbox}
-          subject={subject}
-          onPressLabel={() => setIsEditing(true)}
-          onChangeSubject={setSubject}
-          onFinishEditing={() => setIsEditing(false)}
+        <TaskList
+          data={data}
+          onToggleItem={handleToggleTaskItem}
+          onChangeSubject={handleChangeTaskItemSubject}
+          onFinishEditing={handleFinishEditingTaskItem}
+          onPressLabel={handlePressTaskItemLabel}
+          onRemoveItem={handleRemoveItem}
+          editingItemId={editingItemId}
         />
         <ThemeToggle />
       </VStack>
