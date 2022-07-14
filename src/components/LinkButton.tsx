3052af8c84db477e90dc9b253e1e87199c6adee7
Victor Garcia Mestre <vgmestre@gmail.com>
2022-07-14 23:14:53 +0200

    about screen

---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,16 @@
+import React, { useCallback } from 'react'
+import * as Linking from 'expo-linking'
+import { Button, IButtonProps } from 'native-base'
+
+interface Props extends IButtonProps {
+  href: string
+}
+const LinkButton = ({ href, ...props }: Props) => {
+  const handlePress = useCallback(() => {
+    Linking.openURL(href)
+  }, [href])
+
+  return <Button {...props} onPress={handlePress}></Button>
+}
+
+export default LinkButton
--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,13 +1,69 @@
 import React from 'react'
-import { Box, Text, VStack } from 'native-base'
+import {
+  ScrollView,
+  Box,
+  Text,
+  VStack,
+  Icon,
+  Image,
+  useColorModeValue,
+  useColorMode
+} from 'native-base'
+import { Feather } from '@expo/vector-icons'
+import AnimatedColorBox from '../components/AnimatedColorBox'
+import Navbar from '../components/Navbar'
+import Masthead from '../components/Masthead'
+import LinkButton from '../components/LinkButton'
 
 const About = () => {
   return (
-    <VStack flex={1}>
-      <Box>
-        <Text>About</Text>
-      </Box>
-    </VStack>
+    <AnimatedColorBox
+      flex={1}
+      bg={useColorModeValue('warmGray.50', 'warmGray.900')}
+      w="full"
+    >
+      <Masthead
+        title="About this app"
+        image={require('../../assets/about-masthead.png')}
+      >
+        <Navbar />
+      </Masthead>
+      <ScrollView
+        borderTopLeftRadius="20px"
+        borderTopRightRadius="20px"
+        bg={useColorModeValue('warmGray.50', 'primary.900')}
+        mt="-20px"
+        pt="30px"
+        p={4}
+      >
+        <VStack flex={1} space={4}>
+          <Box alignItems="center">
+            <Image
+              source={require('../../assets/profile-image.png')}
+              borderRadius="full"
+              resizeMode="cover"
+              w={120}
+              h={120}
+              alt="author"
+            />
+          </Box>
+          <Text fontSize="md" w="full">
+            This is a to-do list app made in React Native!
+          </Text>
+          <LinkButton
+            colorScheme="red"
+            size="lg"
+            borderRadius="full"
+            href="https://github.com/vgarmes/yarukoto"
+            leftIcon={
+              <Icon as={Feather} name="github" size="sm" opacity={0.5} />
+            }
+          >
+            See source code
+          </LinkButton>
+        </VStack>
+      </ScrollView>
+    </AnimatedColorBox>
   )
 }
 
